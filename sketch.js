88109df1d8cfcd114034992ca75c743222f01819
Holly Millea <hollymillea@gmail.com>
2024-08-14 15:52:07 +0100

    Uncomment line 60 to produce the frames for the video. Then run the ffmpeg command to create the video.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
+// RUN THIS LINE TO PUT THE FRAMES BACK TOGETHER
+// ffmpeg -r 5 -f image2 -s 794x1121 -i frame_%04d.png -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -vcodec libx264 -crf 25 -pix_fmt yuv420p animation.mp4
+// (when in the same directory as the output images)
+
+let totalFrames = 150; // Total number of frames to capture
+let currentFrame = 0;
+
 function setup() {
-  createCanvas(794, 1121);
-  noLoop(); // Only draw once
+  createCanvas(400, 400);
   noStroke(); // Remove border from circles
 }
 
@@ -9,14 +15,14 @@ function draw() {
   let gridSize = 5; // Size of the grid cells for the first layer
   let circleSize; // Variable to store the size of each circle
   let noiseZoom = 0.001;
-  let time = frameCount * 0.05; // Time variable to animate noise
+  let time = currentFrame * 0.02; // Time variable to animate noise
    
   let black = color(58, 56, 56);
   let color1 = color(135, 56, 45);
   let color2 = color(136, 8, 8);
 
-  const marginX = 50;
-  const marginY = 80;
+  const marginX = 10;
+  const marginY = 10;
   
   // First layer of circles
   for (let y = marginY; y < (height - marginY); y += gridSize) {
@@ -26,10 +32,6 @@ function draw() {
       noiseVal = transformNoise(noiseVal);
       
       circleSize = map(noiseVal, 0, 1, 0, gridSize);
-
-    //   if (circleSize < 0.5) continue;
-
-    //   fill(black);
       noFill();
       stroke(black);
       ellipse(x, y, circleSize, circleSize);
@@ -47,49 +49,37 @@ function draw() {
       let noiseVal2 = transformNoise(noiseVal);
       
       circleSize = map(noiseVal2, 0, 1, 0, gridSize);
-
-    //   if (circleSize < 1) continue;
       
       fill(color2);
       stroke(black);
       ellipse(x + gridSize / 2, y + gridSize / 2, circleSize, circleSize);
     }
   }
+  
+  // Save the current frame as an image
+  // saveFrame();
 
-//   // Second layer of circles
-//   for (let y = marginY; y < height-marginY; y += gridSize) {
-//     for (let x = marginX; x < width-marginX; x += gridSize) {
-//       let noiseVal = noise(x * noiseZoom, y * noiseZoom, time);
-//       let noiseVal2 = transformNoise(noiseVal, 0.42);
-      
-//       circleSize = map(noiseVal2, 0, 1, 0, gridSize);
+  console.log('Frame ' + currentFrame + ' saved');
 
-//       if (circleSize < 1) continue;
-      
-//       fill(color1);
-//       stroke(black);
-//       ellipse(x + gridSize / 2, y + gridSize / 2, circleSize, circleSize);
-//     }
-//   }
+  // Stop the loop when the desired number of frames is reached
+  if (currentFrame >= totalFrames) {
+    noLoop();
+  }
+
+  currentFrame++;
+}
+
+function saveFrame() {
+  let frameName = nf(currentFrame, 4); // Name the frame with leading zeros
+  saveCanvas('frame_' + frameName, 'png'); // Save the frame as a PNG
 }
 
 function transformNoise(x, midPoint) {
-  // How long should the flat bits at the top be?
-//   const xHigh = 0.2;
-  
-  // How long should the flat bits at the bottom be?
   const xLow = 0.05;
-
   const curveLength = 0.05;
   
-  // What range are we extracting?
   if (!midPoint) midPoint = 0.5;
   
-  // We want our curve to start at 'start'
-  // It should then dip down until it gets to dipStop
-  // Then it should sit at value 0
-  // At dipStart it should then curve upwards again
-  // And stop at end at value 1
   const dipStop = midPoint - (xLow / 2);
   const start = dipStop - curveLength;
   const dipStart = dipStop + (xLow / 2);
@@ -101,14 +91,12 @@ function transformNoise(x, midPoint) {
     y = 1;
   }
   else if (x >= start && x <= dipStop) {
-    // Cosine graph starting at xHigh, ending at dipStop
     y = mapCos(x, start, dipStop, 0, 1);
   }
   else if (x > dipStop && x < dipStart) {
     y = 0;
   }
   else if (x > dipStart && x <= end) {
-    // Cos slope going up
     y = 1 - mapCos(x, dipStart, end, 0, 1);
   }
   else {
@@ -119,14 +107,8 @@ function transformNoise(x, midPoint) {
 }
 
 function mapCos(x, xMin, xMax, yMin, yMax) {
-  // The downhill bit of cos(x) goes between x = 0 and x = pi
   let inputX = map(x, xMin, xMax, 0, PI);
-  // let inputX = (x - xMin) * (2*PI / (xMax - xMin));
-  
   let outputY = cos(inputX);
-  
-  // Map y between the values we want
   let y = map(outputY, -1, 1, 0, 1);
-  
   return y;
 }
